5fb9a6f80c2c1efbb5554603d9a5da459bc15a0b
Sergey Kasyanov <skasmail@gmail.com>
2023-03-22 09:46:26 +0400

    fix model attributes casting autcompletion

--- src/extension/providers/completions/php/attribute-cast.ts
+++ src/extension/providers/completions/php/attribute-cast.ts
@@ -3,7 +3,7 @@ import { Model } from "../../../../domain/entities/classes/model";
 import { Store } from "../../../../domain/services/store";
 import { awaitsCompletions } from "../../../helpers/awaits-completions";
 
-const CASTS_PROPERTY = /(protected|public)\s+(array\s+){0,1}\$casts\s*=\s*(\[|array\()(\s*[\'\"]\w+[\'\"]\s*=>\s*(([\'\"][\w]+[\'\"])|[\w\:]+)\s*,)*\s*[\'\"]\w+[\'\"]\s*=>\s*[\'\"]/g;
+const CASTS_PROPERTY = /(protected|public)\s+(array\s+){0,1}\$casts\s*=\s*(\[|array\()(\s*[\'\"]\w+[\'\"]\s*=>\s*(([\'\"]\w*[\'\"])|[\w\:]+)\s*,)*\s*[\'\"]\w+[\'\"]\s*=>\s*[\'\"]/g;
 const CAST_NAME_PART = /^\w*$/;
 const CAST_NAME = /\w+/;
 
