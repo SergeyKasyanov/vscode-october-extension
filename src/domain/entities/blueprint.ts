78f869d37f145b00b8b8bb64d5b73e87b43b0621
Sergey Kasyanov <skasmail@gmail.com>
2023-04-25 11:45:25 +0400

    stubs

---
+++ src/domain/entities/blueprint.ts
@@ -0,0 +1,3 @@
+export class Blueprint {
+
+}
---
+++ src/domain/services/indexer/tailor-indexer.ts
@@ -0,0 +1,13 @@
+import { Store } from "../store";
+
+export class TailorIndexer {
+    private projectPath: string | undefined;
+
+    constructor(
+        private store: Store
+    ) { }
+
+    index(projectPath: string) {
+
+    }
+}
