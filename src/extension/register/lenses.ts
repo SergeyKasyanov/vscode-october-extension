e6bce30c89f1eef23b50776b90a4c054acb948ca
Sergey Kasyanov <skasmail@gmail.com>
2023-04-25 13:51:18 +0400

    migration version lens

--- src/extension/register/lenses.ts
+++ src/extension/register/lenses.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import { themeFileSelector } from "../helpers/file-selectors";
 import { registerControllerViewLensProvider } from "../providers/lenses/controller-view";
 import { registerMigrationModelLensProvider } from "../providers/lenses/migration-models";
+import { registerMigrationVersionLensProvider } from "../providers/lenses/migration-version";
 import { registerModelMigrationsLensProvider } from "../providers/lenses/model-migrations";
 import { ThemeFileReferencesLens } from "../providers/lenses/theme-file-references-lens";
 
@@ -9,6 +10,7 @@ export function registerCodeLenses(context: vscode.ExtensionContext) {
     registerModelMigrationsLensProvider(context);
     registerMigrationModelLensProvider(context);
     registerControllerViewLensProvider(context);
+    registerMigrationVersionLensProvider(context);
 
     vscode.languages.registerCodeLensProvider(themeFileSelector, new ThemeFileReferencesLens());
 }
