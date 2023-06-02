c1a131d2fb8ac5ff7266c1825bf2dafcb3e8d03b
Sergey Kasyanov <skasmail@gmail.com>
2023-06-02 15:54:13 +0300

    fix permission completions

--- src/extension/providers/completions/php/permission.ts
+++ src/extension/providers/completions/php/permission.ts
@@ -5,7 +5,7 @@ import { CompletionItem } from "../../../factories/completion-item";
 import { awaitsCompletions } from "../../../helpers/awaits-completions";
 
 const REQUIRED_PERMISSIONS = /\$requiredPermissions\s+\=\s+(\[|(array\())/g;
-const CHECK_METHOD = /(\->hasAccess\s*\(|\->hasAnyAccess\s*\(|\->hasPermission\s*\()\s*(\[|(array\()){0,1}/g;
+const CHECK_METHOD = /(\:\:userHasAccess|\:\:userHasPermission|\->hasAccess|\->hasAnyAccess|\->hasPermission)\s*\(\s*(\[|(array\()){0,1}/g;
 const PERMISSION_PART = /^\s*[\'\"]([\w\.\-\_\:]*[\'\"]\s*,\s*[\'\"])*[\w\.\-\_\:]*$/;
 const PERMISSION_VALUE = /[\w\.\-\_\:]+/;
 
@@ -32,21 +32,21 @@ export class Permission implements vscode.CompletionItemProvider {
         position: vscode.Position
     ): vscode.ProviderResult<vscode.CompletionItem[] | vscode.CompletionList<vscode.CompletionItem>> {
 
-        const entity = Store.instance.findEntity(document.fileName);
-        if (!entity) {
+        const project = Store.instance.findProject(document.fileName);
+        if (!project) {
             return;
         }
 
-        if (!(entity instanceof Controller && awaitsCompletions(
+        if (!awaitsCompletions(
             document.getText(),
             document.offsetAt(position),
             [REQUIRED_PERMISSIONS, CHECK_METHOD],
             PERMISSION_PART
-        ))) {
+        )) {
             return;
         }
 
-        return entity.owner.project.permissions.map(perm => {
+        return project.permissions.map(perm => {
             const item = CompletionItem.fromPermission(perm);
             item.range = document.getWordRangeAtPosition(position, PERMISSION_VALUE);
 
